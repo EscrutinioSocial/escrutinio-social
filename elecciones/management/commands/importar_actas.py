9b2d0a39361a0e41b1f781c63a96d3c5f22711cd
avdata99 <andres@data99.com.ar>
2019-05-07 18:22:18 -0300

    vagancia de gaitan

--- elecciones/management/commands/importar_actas.py
+++ elecciones/management/commands/importar_actas.py
@@ -18,7 +18,8 @@ class Command(escrutinio_socialBaseCommand):
 
     def handle(self, *args, **options):
 
-        for imap in settings.IMAPs:
+        imaps = settings.IMAPS
+        for imap in imaps:
         
             imapper = easyimap.connect(imap['host'], imap['user'], imap['pass'], imap['mailbox'])
             self.success('Loggueado')
