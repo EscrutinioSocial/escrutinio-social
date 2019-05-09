89f8aa1c9c87d527b9518261cd23eb7ca41c2232
Martin Gaitan <mgaitan@onapsis.com>
2019-05-09 17:28:36 -0300

    el orden del dispatcher de actas a cargar prioriza secciones más grandes (luego del orden)

--- fiscales/views.py
+++ fiscales/views.py
@@ -261,10 +261,10 @@ class MisDatosUpdate(ConContactosMixin, UpdateView, BaseFiscal):
 
 @staff_member_required
 def elegir_acta_a_cargar(request):
-
     # se eligen mesas que nunca se intentaron cargar o que se asignaron a
-    mesas = Mesa.con_carga_pendiente().order_by('orden_de_carga')
-
+    mesas = Mesa.con_carga_pendiente().order_by(
+        'orden_de_carga', '-lugar_votacion__circuito__seccion__electores'
+    )
     if mesas.exists():
         mesa = mesas[0]
         # se marca que se inicia una carga
