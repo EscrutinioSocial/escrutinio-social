21ad27bb90aa860ffb3f63acbf1558c7547d1209
avdata99 <andres@data99.com.ar>
2019-05-14 17:44:19 -0300

    buscar por remitente

--- adjuntos/admin.py
+++ adjuntos/admin.py
@@ -7,7 +7,7 @@ from django_admin_row_actions import AdminRowActionsMixin
 class AttachmentAdmin(AdminRowActionsMixin, admin.ModelAdmin):
     list_display = ('email', 'problema', 'mesa', 'foto', 'foto_edited', 'taken')
     list_filter = ('problema',)
-    search_fields = ('mesa__numero', 'email__from_address')
+    search_fields = ('mesa__numero', 'email__from_address', 'email__title', 'email__body')
 
     def get_row_actions(self, obj):
         row_actions = []
