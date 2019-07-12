0e5ad65465720c683a9a30e8f929bfbf51db8ddb
cbarbosa <cbarbosa@mzingenieria.com.ar>
2019-07-12 18:30:57 +0000

    Creating CI/CD Pipeline
    Creating Build and Deploy Scripts
    
    Adapting docker-compose
    
    Defining new entrypoint for container
    
    Creating templates for ECS

---
+++ ci/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+# We first login to the registry
+$(aws ecr get-login --no-include-email --region ${REGION})
+
+# build image and push image
+docker build -t "${CONTAINER_IMAGE}" .
+docker push "${CONTAINER_IMAGE}"
+
+echo "${CONTAINER_IMAGE}" > container_image
---
+++ ci/deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+export CONTAINER_IMAGE="$(cat container_image)"
+PATHDIR='./ci/ecs_templates'
+
+# MASTER
+################################################
+if [[ "${CI_COMMIT_REF_NAME}" = "master" ]] ; then
+    export ENVIRONMENT="PROD"
+    export ECS_CLUSTER="ECS-PROD"
+    echo
+    echo "Deploying MASTER Branch to PROD"
+    echo
+
+# DEVELOPMENT
+################################################
+elif [[ "${CI_COMMIT_REF_NAME}" = "setup-ci" ]] ; then
+    export ENVIRONMENT="DEV"
+    export ECS_CLUSTER="Convencer-Test"
+
+    echo
+    echo "Deploying DEVELOP Branch to DEV Cluster"
+    echo
+
+else
+    echo
+    echo "Only deploy master or develop branch"
+    echo
+    exit 2
+fi
+
+envsubst < ${PATHDIR}/escrutinio-paralelo-svc.json.template > ${PATHDIR}/escrutinio-paralelo-svc.json
+aws ecs update-service --cli-input-json file://${PATHDIR}/escrutinio-paralelo-svc.json --force-new-deployment
