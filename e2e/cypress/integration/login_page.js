cbcc5a3c5aa19304992069842401a1b36a8c1184
mbrassesco <m.virgi@gmail.com>
2019-07-12 02:06:35 -0300

    agrego fixture de usuarios

--- e2e/cypress/integration/login_page.js
+++ e2e/cypress/integration/login_page.js
@@ -1,21 +1,23 @@
 describe('Pagina de Login', () => {
-  it('chequear web login', () => {
-    cy.visit('/login/');
-    cy.contains('Nombre de usuario o DNI');
-  });
+    it('chequear web login', () => {
+        cy.visit('/login/');
+        cy.contains('Nombre de usuario o DNI');
+    });
 
-  it('chequear web fiscales', () => {
-    cy.visit('/quiero-ser-fiscal/');
-    cy.contains('Quiero ser fiscal de las PASO Nacional 2019');
-  });
+    it('chequear web fiscales', () => {
+        cy.visit('/quiero-ser-fiscal/');
+        cy.contains('Quiero ser fiscal de las PASO Nacional 2019');
+    });
 
-  it('login', () => {
-    cy.visit('/login/');
-    cy.get('#id_username').type('admin');
-    cy.get('#id_password').first().focus();
-    cy.get('#id_password').type('admin');
-    cy.contains('Identificarse').click();
-    cy.contains('Cargar resultados de actas');
-  });
+    it('login', () => {
+        cy.visit('/login/');
+        cy.fixture('usuarios').then((user) => {
+            cy.get('#id_username').type(user.admin_username);
+            cy.get('#id_password').first().focus();
+            cy.get('#id_password').type(user.admin_pass);
+        });
+        cy.contains('Identificarse').click();
+        cy.contains('Cargar resultados de actas');
+    });
 
 });
