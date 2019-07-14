ba2be4c89341c5c9b501e5a264e0867963ab2425
ferclaverino <fernando.claverino@gmail.com>
2019-07-13 21:25:57 -0300

    arreglar orden del tab

--- adjuntos/views.py
+++ adjuntos/views.py
@@ -74,7 +74,6 @@ class IdentificacionCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context['attachment'] = self.attachment
         context['form_problema'] = IdentificacionProblemaForm()
-        context['button_tabindex'] = 5
         return context
 
     def form_valid(self, form):
