2447ea29473f96b99bcc50ee581c4b3365d6a84e
ferclaverino <fernando.claverino@gmail.com>
2019-07-14 22:02:45 -0300

    arreglar login e2e

--- e2e/cypress/integration/resultados.js
+++ e2e/cypress/integration/resultados.js
@@ -2,7 +2,7 @@ describe('Pagina de resultados', () => {
   beforeEach(() => cy.loginAsAdmin());
 
   it('resultados', () => {
-    cy.visit('/elecciones/resultados/1');
+    cy.visit('/elecciones/resultados-parciales-sin-confirmar/1');
     cy.contains('Resultados para Córdoba');
   });
 
