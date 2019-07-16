2cadca0e0b298c1cff2019b318f8afc6fddd5e05
Martin Gaitan <mgaitan@onapsis.com>
2019-07-16 13:05:36 -0300

    quito el loop infinito. deberia ejecutarse cada algunos segundos via cron

--- adjuntos/management/commands/consolidar_identificaciones_y_cargas.py
+++ adjuntos/management/commands/consolidar_identificaciones_y_cargas.py
@@ -1,11 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
-from adjuntos.models import *
-from adjuntos.consolidacion import *
+from django.core.management.base import BaseCommand
+from adjuntos.consolidacion import consumir_novedades
 import logging
 
 logger = logging.getLogger("e-va")
-UMBRAL_ITERACIONES_NOTIF = 10000
+
 
 class Command(BaseCommand):
     help = "Consolidador asincrónico"
@@ -13,12 +11,5 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
 
-
     def handle(self, *args, **options):
-        iteracion = 0
-        while True:
-            consumir_novedades()
-            if iteracion == UMBRAL_ITERACIONES_NOTIF:
-                logger.debug("Consumiendo novedades.")
-                iteracion = 0
-            iteracion = iteracion + 1
+        consumir_novedades()
