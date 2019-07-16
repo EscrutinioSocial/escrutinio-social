40f7538645d022f32e45acf0c896eb8b404b2be5
Fernando Schapachnik <fernando@schapachnik.com.ar>
2019-07-16 19:29:45 -0300

    Typo.

--- escrutinio_social/settings.py
+++ escrutinio_social/settings.py
@@ -246,14 +246,14 @@ CARACTERISTICA_TELEFONO_DEFAULT = '351'     # CORDOBA
 CARACTERISTICA_DEFAULT = '351'
 
 
-# por defecto no se muestra grafico en la página de resultados
+# Por defecto no se muestra gráfico en la página de resultados
 SHOW_PLOT = False
 
 MIN_COINCIDENCIAS_IDENTIFICACION = 2
 MIN_COINCIDENCIAS_CARGAS = 2
 
-# tiempo en segundos que se espera entre
-# recalculo de consolidaciones de identificacion y carga
+# Tiempo en segundos que se espera entre
+# recálculo de consolidaciones de identificación y carga
 PAUSA_CONSOLIDACION = 15
 
 
