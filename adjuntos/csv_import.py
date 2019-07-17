83cb401586d0322885a25378784e043742733f51
Virginia Barros <virginianbarros@gmail.com>
2019-07-17 11:53:24 -0300

    Agregando tests al csv import

--- adjuntos/csv_import.py
+++ adjuntos/csv_import.py
@@ -8,8 +8,9 @@ from fiscales.models import Fiscal
 
 COLUMNAS_DEFAULT = [('seccion', False), ('distrito', False), ('circuito', False), ('nro de mesa', False),
                     ('nro de lista', False), ('presidente y vice', True), ('gobernador y vice', True),
-                    ('senadores nacionales', True), ('diputados nacionales', True), ('legisladores provinciales', True),
-                    ('senadores provinciales', True), ('diputados provinciales', True),
+                    ('senadores nacionales', True), ('diputados nacionales', True),
+                    ('legisladores provinciales', True), ('senadores provinciales', True),
+                    ('diputados provinciales', True),
                     ('intendentes, concejales y consejeros escolares', True),
                     ('cantidad de electores del padron', False), ('cantidad de sobres en la urna', False),
                     ('acta arreglada', False)]
@@ -69,7 +70,6 @@ class CSVImporter:
             raise e
         # para manejar cualquier otro tipo de error
         except Exception as e:
-            print(str(e))
             raise FormatoArchivoInvalidoError('No es un csv válido')
 
     def validar_columnas(self):
@@ -85,7 +85,8 @@ class CSVImporter:
             faltantes = [columna for columna in headers if columna not in self.df.columns]
             raise ColumnasInvalidasError(f'Faltan las columnas: {faltantes} en el archivo')
         # las columnas duplicadas en Panda se especifican como ‘X’, ‘X.1’, …’X.N’
-        columnas_candidatas = [columna.replace('.1', '') for columna in self.df.columns if columna.endswith('.1')]
+        columnas_candidatas = [columna.replace('.1', '') for columna in self.df.columns
+                               if columna.endswith('.1')]
         columnas_duplicadas = any(elem in columnas_candidatas for elem in headers)
         if columnas_duplicadas:
             raise ColumnasInvalidasError('Hay columnas duplicadas en el archivo')
@@ -101,10 +102,12 @@ class CSVImporter:
 
         for mesa in mesa_circuito_seccion_distrito:
             try:
-                match_mesa = Mesa.obtener_mesa_en_circuito_seccion_distrito(mesa[2], mesa[1], mesa[0], mesa[3])
+                match_mesa = Mesa.obtener_mesa_en_circuito_seccion_distrito(mesa[2], mesa[1],
+                                                                            mesa[0], mesa[3])
             except Mesa.DoesNotExist:
                 raise DatosInvalidosError(
-                    f'No existe mesa: {mesa[2]} en circuito: {mesa[1]}, sección: {mesa[0]} y distrito: {mesa[3]}')
+                    f'No existe mesa: {mesa[2]} en circuito: {mesa[1]}, sección: {mesa[0]} y '
+                    f'distrito: {mesa[3]}')
             self.mesas_matches[mesa] = match_mesa
             self.mesas.append(match_mesa)
 
@@ -141,15 +144,19 @@ class CSVImporter:
                                 cantidad_votos = fila[mesa_columna]
                                 opcion = fila['nro de lista']
                                 fila_analizada = FilaCSVImporter(mesa[0], mesa[1], mesa[2], mesa[3])
-                                # buscamos este nro de lista dentro de las opciones asociadas a esta categoria
+                                # buscamos este nro de lista dentro de las opciones asociadas a
+                                # esta categoria
                                 match_opcion = [una_opcion for una_opcion in categoria_bd.opciones.all()
-                                                if una_opcion.codigo.strip().lower() == str(opcion).strip().lower()]
+                                                if una_opcion.codigo and una_opcion.codigo.strip().lower()
+                                                == str(opcion).strip().lower()]
                                 opcion_bd = match_opcion[0] if len(match_opcion) > 0 else None
                                 if not opcion_bd:
-                                    raise DatosInvalidosError(f'El número de lista {opcion} no fue encontrado asociado la '
-                                                              f'categoria {categoria_bd.nombre}, '
-                                                              f'revise que sea el correcto.')
-                                opcion_categoria = opcion_bd.categoriaopcion_set.filter(categoria=categoria_bd).first()
+                                    raise DatosInvalidosError(f'El número de lista {opcion} no fue '
+                                                              f'encontrado asociado la categoria '
+                                                              f'{categoria_bd.nombre}, revise que sea el '
+                                                              f'correcto.')
+                                opcion_categoria = opcion_bd.categoriaopcion_set\
+                                    .filter(categoria=categoria_bd).first()
                                 if opcion_categoria.prioritaria:
                                     if not carga_parcial:
                                         carga_parcial = Carga.objects.create(
@@ -171,7 +178,8 @@ class CSVImporter:
                                 voto = VotoMesaReportado(carga=carga, votos=cantidad_votos, opcion=opcion_bd)
                                 voto.save()
                         else:
-                            raise DatosInvalidosError(f'Faltan datos en el archivo de la siguiente categoria: {categoria_bd.nombre}.')
+                            raise DatosInvalidosError(f'Faltan datos en el archivo de la siguiente '
+                                                      f'categoria: {categoria_bd.nombre}.')
                         # si todas las opciones fueron prioritarias entonces la carga es total, sino parcial
                         # TODO revisar esto de actualizar firma si es necesario para ambas cargas
                         #carga_parcial.actualizar_firma()
@@ -181,12 +189,14 @@ class CSVImporter:
             # fixme ver mejor forma de manejar estos errores
             if 'votomesareportado_votos_check' in str(e):
                 raise DatosInvalidosError(
-                    f'Los resultados deben ser números positivos. Revise las filas correspondientes a {fila_analizada}')
-            raise DatosInvalidosError(f'Error al guardar los resultados. Revise las filas correspondientes a  {fila_analizada}')
+                    f'Los resultados deben ser números positivos. Revise las filas correspondientes '
+                    f'a {fila_analizada}')
+            raise DatosInvalidosError(f'Error al guardar los resultados. Revise las filas correspondientes '
+                                      f'a  {fila_analizada}')
         except ValueError as e:
-            print(e)
             raise DatosInvalidosError(
-                f'Revise que los datos de resultados sean numéricos. Revise las filas correspondientes a  {fila_analizada}')
+                f'Revise que los datos de resultados sean numéricos. Revise las filas correspondientes '
+                f'a  {fila_analizada}')
         except Exception as e:
             raise e
 
@@ -199,4 +209,5 @@ class FilaCSVImporter:
         self.distrito = distrito
 
     def __str__(self):
-        return f"Mesa: {self.mesa} - Sección: {self.seccion} - Circuito: {self.circuito} - Distrito: {self.distrito}"
+        return f"Mesa: {self.mesa} - Sección: {self.seccion} - Circuito: {self.circuito} - " \
+            f"Distrito: {self.distrito}"
--- adjuntos/tests/test_csv_import.py
+++ adjuntos/tests/test_csv_import.py
@@ -4,10 +4,24 @@ import os
 from django.http import Http404
 
 from adjuntos.csv_import import ColumnasInvalidasError, CSVImporter, DatosInvalidosError
-from elecciones.tests.factories import DistritoFactory, SeccionFactory, CircuitoFactory, MesaFactory, CategoriaFactory, \
-    OpcionFactory, CategoriaOpcionFactory, MesaCategoriaFactory, FiscalFactory, UserFactory
+from elecciones.models import Carga
+from elecciones.tests.factories import (
+    DistritoFactory,
+    SeccionFactory,
+    CircuitoFactory,
+    MesaFactory,
+    CategoriaFactory,
+    OpcionFactory,
+    CategoriaOpcionFactory,
+    MesaCategoriaFactory,
+    FiscalFactory,
+    UserFactory)
 
 PATH_ARCHIVOS_TEST = os.path.dirname(os.path.abspath(__file__)) + '/archivos/'
+CATEGORIAS = [('Presidente y vice,Gobernador y vice', True),
+              ('Intendentes, Concejales y Consejeros Escolares', False), ('Legisladores Provinciales', True),
+              ('Senadores Nacionales', True), ('Diputados Nacionales', True),
+              ('Senadores Provinciales', True), ('Diputados Provinciales', True)]
 
 
 def test_validar_csv_faltan_columnas():
@@ -51,8 +65,56 @@ def test_procesar_csv_categorias_faltantes_en_archivo(db):
     o2 = OpcionFactory(orden=3, codigo='A')
     o3 = OpcionFactory(orden=2, codigo='B')
     c = CategoriaFactory(opciones=[o2, o3], nombre='Presidente y vice')
-    o1 = CategoriaOpcionFactory(categoria=c, opcion__orden=1, prioritaria=True)
+    CategoriaOpcionFactory(categoria=c, opcion__orden=1, prioritaria=True)
     MesaCategoriaFactory(mesa=m, categoria=c)
     with pytest.raises(DatosInvalidosError) as e:
         CSVImporter(PATH_ARCHIVOS_TEST + 'info_resultados_negativos.csv', user).procesar()
     assert 'Faltan datos en el archivo de la siguiente categoria' in str(e.value)
+
+
+@pytest.fixture()
+def carga_inicial(db):
+    d1 = DistritoFactory(numero=1)
+    s1 = SeccionFactory(numero=50, distrito=d1)
+    circ = CircuitoFactory(numero='2', seccion=s1)
+    o1 = OpcionFactory(orden=3, codigo='A')
+    o2 = OpcionFactory(orden=2, codigo='B')
+    categorias = []
+    for categoria in CATEGORIAS:
+        categoria_bd = CategoriaFactory(nombre=categoria[0])
+        categorias.append(categoria_bd)
+        CategoriaOpcionFactory(categoria=categoria_bd, opcion__orden=1, prioritaria=categoria[1], opcion=o1)
+        CategoriaOpcionFactory(categoria=categoria_bd, opcion__orden=1, prioritaria=categoria[1], opcion=o2)
+    MesaFactory(numero='4012', lugar_votacion__circuito=circ, electores=100, circuito=circ,
+                categorias=categorias)
+
+
+def test_procesar_csv_resultados_negativos(db, carga_inicial):
+    user = UserFactory()
+    FiscalFactory(user=user)
+    with pytest.raises(DatosInvalidosError) as e:
+        CSVImporter(PATH_ARCHIVOS_TEST + 'info_resultados_negativos.csv', user).procesar()
+    assert 'Los resultados deben ser números positivos' in str(e.value)
+
+
+def test_procesar_csv_opciones_no_encontradas(db, carga_inicial):
+    user = UserFactory()
+    FiscalFactory(user=user)
+    with pytest.raises(DatosInvalidosError) as e:
+        CSVImporter(PATH_ARCHIVOS_TEST + 'opciones_invalidas.csv', user).procesar()
+    assert 'El número de lista C no fue encontrado' in str(e.value)
+
+
+def test_procesar_csv_informacion_valida_genera_resultados(db, carga_inicial):
+    user = UserFactory()
+    FiscalFactory(user=user)
+    CSVImporter(PATH_ARCHIVOS_TEST + 'info_resultados_ok.csv', user).procesar()
+    carga_total = Carga.objects.filter(status=Carga.STATUS.total).all()
+    totales = len([categoria for categoria in CATEGORIAS if not categoria[1]])
+    assert len(carga_total) == totales
+    for total in carga_total:
+        assert total.origen == 'csv'
+    carga_parcial = Carga.objects.filter(status=Carga.STATUS.parcial).all()
+    assert len(carga_parcial) == len(CATEGORIAS) - totales
+    for parcial in carga_parcial:
+        assert parcial.origen == 'csv'
