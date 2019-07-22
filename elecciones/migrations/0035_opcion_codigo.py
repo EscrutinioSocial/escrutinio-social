34076ab70e077357c93b92a718afef067f1185f0
Fernando Schapachnik <fernando@schapachnik.com.ar>
2019-07-21 23:22:37 -0300

    Fix migr.

--- elecciones/migrations/0035_opcion_codigo.py
+++ elecciones/migrations/0035_opcion_codigo.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('elecciones', '0034_auto_20190721_1032'),
+        ('elecciones', '0034_auto_20190721_2211'),
     ]
 
     operations = [
