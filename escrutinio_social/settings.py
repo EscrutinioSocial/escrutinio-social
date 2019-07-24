983de222911bc27c92ec49d4ff4c0eabb24a1aac
Carlos Lombardi <carlombardi@gmail.com>
2019-07-24 16:35:28 -0300

    scheduling revisited - ajustes al modelo

--- escrutinio_social/settings.py
+++ escrutinio_social/settings.py
@@ -315,10 +315,13 @@ MESA_TAKE_WAIT_TIME = 2  # En minutos
 
 # Prioridades standard, a usar si no se definen prioridades específicas
 # para una categoría o circuito
-PRIORIDADES_STANDARD = [
+PRIORIDADES_STANDARD_CIRCUITO = [
     {'desde': 0, 'hasta': 2, 'prioridad': 2},
     {'desde': 2, 'hasta': 10, 'prioridad': 20},
-    {'desde': 20, 'hasta': 100, 'prioridad': 200},
+    {'desde': 20, 'hasta': 100, 'prioridad': 100},
+]
+PRIORIDADES_STANDARD_CATEGORIA = [
+    {'desde': 0, 'hasta': 100, 'prioridad': 100},
 ]
 
 # Las siguientes constantes definen los criterios de filtro
