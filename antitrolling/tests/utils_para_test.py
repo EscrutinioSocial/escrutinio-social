5acf992c6d27c1df311f549a1a01117b36537a24
Martin Gaitan <mgaitan@onapsis.com>
2019-07-26 00:28:09 -0300

    arreglo funcion helper nueva_carga

--- antitrolling/tests/utils_para_test.py
+++ antitrolling/tests/utils_para_test.py
@@ -1,7 +1,13 @@
 from elecciones.models import Carga
 from elecciones.tests.factories import (
-    UserFactory, FiscalFactory, IdentificacionFactory,
-    OpcionFactory, CategoriaFactory, CargaFactory, VotoMesaReportadoFactory
+    CargaFactory,
+    CategoriaFactory,
+    CategoriaOpcionFactory,
+    FiscalFactory,
+    IdentificacionFactory,
+    OpcionFactory,
+    UserFactory,
+    VotoMesaReportadoFactory,
 )
 
 
@@ -28,16 +34,18 @@ def reportar_problema_attachment(attach, fiscal):
     )
 
 
-def nueva_categoria(nombres_opciones, prior=False):
-    opciones = list(map(lambda n: OpcionFactory(nombre=n), nombres_opciones))
-    return CategoriaFactory(opciones=opciones, prioritaria=prior)
+def nueva_categoria(nombres_opciones, prioritaria=False):
+    categoria = CategoriaFactory(opciones=[])   # sin opciones para crearlas ad hoc
+    for nombre in nombres_opciones:
+        CategoriaOpcionFactory(categoria=categoria, opcion__nombre=nombre, prioritaria=prioritaria)
+    return categoria
 
 
 def nueva_carga(mesa_categoria, fiscal, votos_opciones, tipo_carga=Carga.TIPOS.total):
-  carga = CargaFactory(mesa_categoria=mesa_categoria, fiscal=fiscal, tipo=tipo_carga)
-  for opcionVoto, cantidadVotos in zip(mesa_categoria.categoria.opciones.order_by('nombre'), votos_opciones):
-    VotoMesaReportadoFactory(carga=carga, opcion=opcionVoto, votos=cantidadVotos)
-  return carga
+    carga = CargaFactory(mesa_categoria=mesa_categoria, fiscal=fiscal, tipo=tipo_carga)
+    for opcionVoto, cantidadVotos in zip(mesa_categoria.categoria.opciones.order_by('nombre'), votos_opciones):
+        VotoMesaReportadoFactory(carga=carga, opcion=opcionVoto, votos=cantidadVotos)
+    return carga
 
 
 def reportar_problema_mesa_categoria(mesa_categoria, fiscal):
