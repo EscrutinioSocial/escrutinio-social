2f0dbeead5f8ccd52e0b74db90f323399db7b5c6
ferclaverino <fernando.claverino@gmail.com>
2019-07-26 20:34:25 -0300

    arreglar sidenav para usuario validador

--- custom_templates/static/js/sidenav.js
+++ custom_templates/static/js/sidenav.js
@@ -5,7 +5,10 @@ $(document).ready(function() {
   // Si el menu solo contiene actuar sobre acta
   // se oculta side nav
   if ($sideNavItems.length === 3 && $sideNavItem.length) {
-    $('.sidenav').removeClass('sidenav-fixed');
+    sessionStorage.setItem('viewflow_site_drawer_state', 'closed');
+    $('.sidenav')
+      .removeClass('sidenav-fixed')
+      .removeAttr('style');
     $('main').css('margin-left', 0);
   }
 
