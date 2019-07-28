17530a33cf57637b713227342b9305fdeb0fa242
Carlos Lombardi <carlombardi@gmail.com>
2019-07-28 13:05:42 -0300

    scheduling revisited - test prioridades seccion prioritaria andando

--- scheduling/tests/test_models.py
+++ scheduling/tests/test_models.py
@@ -36,37 +36,37 @@ def definir_prioridades_seccion_categoria(settings):
         {'desde_proporcion': 0, 'hasta_proporcion': 100, 'prioridad': 100},
     ]
 
-    hudson = SeccionFactory(nombre="Hudson")
-    solano = SeccionFactory(nombre="Solano")
-    ezpeleta = SeccionFactory(nombre="Ezpeleta")
-    platanos = SeccionFactory(nombre="Platanos")
-    PrioridadSchedulingFactory(seccion=hudson, desde_proporcion=50, hasta_proporcion=100, prioridad=250)
-    PrioridadSchedulingFactory(seccion=hudson, desde_proporcion=30, hasta_proporcion=50, prioridad=120)
-    PrioridadSchedulingFactory(seccion=hudson, desde_proporcion=10, hasta_proporcion=30, prioridad=80)
-    PrioridadSchedulingFactory(seccion=hudson, desde_proporcion=0,
+    seccion_cuatro_prioridades = SeccionFactory(nombre="Cuatro prioridades")
+    seccion_dos_cantidades = SeccionFactory(nombre="Dos cantidades")
+    seccion_prioritaria = SeccionFactory(nombre="Prioritaria")
+    seccion_standard = SeccionFactory(nombre="Standard")
+    PrioridadSchedulingFactory(seccion=seccion_cuatro_prioridades, desde_proporcion=50, hasta_proporcion=100, prioridad=250)
+    PrioridadSchedulingFactory(seccion=seccion_cuatro_prioridades, desde_proporcion=30, hasta_proporcion=50, prioridad=120)
+    PrioridadSchedulingFactory(seccion=seccion_cuatro_prioridades, desde_proporcion=10, hasta_proporcion=30, prioridad=80)
+    PrioridadSchedulingFactory(seccion=seccion_cuatro_prioridades, desde_proporcion=0,
                                hasta_proporcion=10, hasta_cantidad=12, prioridad=20)
-    PrioridadSchedulingFactory(seccion=solano, desde_proporcion=0,
+    PrioridadSchedulingFactory(seccion=seccion_dos_cantidades, desde_proporcion=0,
                                hasta_proporcion=2, hasta_cantidad=7, prioridad=5)
-    PrioridadSchedulingFactory(seccion=solano, desde_proporcion=2,
+    PrioridadSchedulingFactory(seccion=seccion_dos_cantidades, desde_proporcion=2,
                                hasta_proporcion=10, hasta_cantidad=20, prioridad=25)
-    PrioridadSchedulingFactory(seccion=solano, desde_proporcion=10, hasta_proporcion=100, prioridad=110)
-    PrioridadSchedulingFactory(seccion=ezpeleta, desde_proporcion=0,
+    PrioridadSchedulingFactory(seccion=seccion_dos_cantidades, desde_proporcion=10, hasta_proporcion=100, prioridad=110)
+    PrioridadSchedulingFactory(seccion=seccion_prioritaria, desde_proporcion=0,
                                hasta_proporcion=2, hasta_cantidad=7, prioridad=10)
-    PrioridadSchedulingFactory(seccion=ezpeleta, desde_proporcion=2, hasta_proporcion=10, prioridad=50)
-    PrioridadSchedulingFactory(seccion=ezpeleta, desde_proporcion=10, hasta_proporcion=100, prioridad=80)
+    PrioridadSchedulingFactory(seccion=seccion_prioritaria, desde_proporcion=2, hasta_proporcion=10, prioridad=50)
+    PrioridadSchedulingFactory(seccion=seccion_prioritaria, desde_proporcion=10, hasta_proporcion=100, prioridad=80)
 
     pv = CategoriaFactory(nombre="PV")
     gv = CategoriaFactory(nombre="GV")
-    concejales = CategoriaFactory(nombre="Concejales")
+    categoria_standard = CategoriaFactory(nombre="Standard")
     PrioridadSchedulingFactory(categoria=pv, desde_proporcion=0, hasta_proporcion=100, prioridad=5)
     PrioridadSchedulingFactory(categoria=gv, desde_proporcion=0, hasta_proporcion=100, prioridad=30)
 
     # tambien hay que definir una mesa en cada seccion, para lo cual tengo que definir
     # circuitos y lugares de votacion
-    crear_mesa(hudson)
-    crear_mesa(solano)
-    crear_mesa(ezpeleta)
-    crear_mesa(platanos)
+    crear_mesa(seccion_cuatro_prioridades)
+    crear_mesa(seccion_dos_cantidades)
+    crear_mesa(seccion_prioritaria)
+    crear_mesa(seccion_standard)
     
 
 def crear_mesa(seccion):
@@ -74,17 +74,17 @@ def crear_mesa(seccion):
     lugar_votacion = LugarVotacionFactory(circuito=circuito)
     return MesaFactory(lugar_votacion=lugar_votacion)
 
-def seccion_hudson():
-    return Seccion.objects.filter(nombre="Hudson")[0]
+def seccion_cuatro_prioridades():
+    return Seccion.objects.filter(nombre="Cuatro prioridades")[0]
 
-def seccion_solano():
-    return Seccion.objects.filter(nombre="Solano")[0]
+def seccion_dos_cantidades():
+    return Seccion.objects.filter(nombre="Dos cantidades")[0]
 
-def seccion_ezpeleta():
-    return Seccion.objects.filter(nombre="Ezpeleta")[0]
+def seccion_prioritaria():
+    return Seccion.objects.filter(nombre="Prioritaria")[0]
 
-def seccion_platanos():
-    return Seccion.objects.filter(nombre="Platanos")[0]
+def seccion_standard():
+    return Seccion.objects.filter(nombre="Standard")[0]
 
 def mesa_en_seccion(seccion):
     return Mesa.objects.filter(lugar_votacion__circuito__seccion=seccion)[0]
@@ -95,14 +95,14 @@ def categoria_pv():
 def categoria_gv():
     return Categoria.objects.filter(nombre="GV")[0]
 
-def categoria_concejales():
-    return Categoria.objects.filter(nombre="Concejales")[0]
+def categoria_standard():
+    return Categoria.objects.filter(nombre="Standard")[0]
 
 
 def test_prioridades_seccion(db, settings):
     definir_prioridades_seccion_categoria(settings)
 
-    mapa = mapa_prioridades_para_seccion(seccion_hudson())
+    mapa = mapa_prioridades_para_seccion(seccion_cuatro_prioridades())
     regis = mapa.registros_ordenados()
     assert len(regis) == 4
     verificar_registro_prioridad(regis[0], 0, 10, 20, 12)
@@ -110,7 +110,7 @@ def test_prioridades_seccion(db, settings):
     verificar_registro_prioridad(regis[2], 30, 50, 120)
     verificar_registro_prioridad(regis[3], 50, 100, 250)
 
-    mapa = mapa_prioridades_para_seccion(seccion_solano())
+    mapa = mapa_prioridades_para_seccion(seccion_dos_cantidades())
     regis = mapa.registros_ordenados()
     assert len(regis) == 3
     verificar_registro_prioridad(regis[0], 0, 2, 5, 7)
@@ -137,7 +137,7 @@ def test_prioridades_categoria(db, settings):
     verificar_registro_prioridad(regis[0], 0, 2, 5)
     verificar_registro_prioridad(regis[1], 2, 100, 30)
 
-    mapa = mapa_prioridades_para_categoria(categoria_concejales())
+    mapa = mapa_prioridades_para_categoria(categoria_standard())
     regis = mapa.registros_ordenados()
     assert len(regis) == 0
 
@@ -160,7 +160,37 @@ def test_prioridades_mesa_categoria_standard(db, settings, proporcion, orden_de_
     definir_prioridades_seccion_categoria(settings)
     
     mesa_categoria = MesaCategoriaFactory(
-        categoria=categoria_concejales(), mesa=mesa_en_seccion(seccion_platanos()))
+        categoria=categoria_standard(), mesa=mesa_en_seccion(seccion_standard()))
+    prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
+
+    assert(prioridades.valor_para(proporcion,orden_de_llegada)) == prioridad
+
+
+    # PrioridadSchedulingFactory(seccion=seccion_prioritaria, desde_proporcion=0,
+    #                            hasta_proporcion=2, hasta_cantidad=7, prioridad=10)
+    # PrioridadSchedulingFactory(seccion=seccion_prioritaria, desde_proporcion=2, hasta_proporcion=10, prioridad=50)
+    # PrioridadSchedulingFactory(seccion=seccion_prioritaria, desde_proporcion=10, hasta_proporcion=100, prioridad=80)
+@pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
+    [0, 1, 1000],
+    [1, 2, 1000],
+    [2, 3, 1000],
+    [6, 7, 1000],
+    [7, 8, 5000],
+    [9, 10, 5000],
+    [10, 11, 8000],
+    [80, 81, 8000],
+])
+def test_prioridades_mesa_categoria_seccion_prioritaria_100(db, settings, proporcion, orden_de_llegada, prioridad):
+    """
+    Prioridades para una MesaCategoria de una seccion prioritaria, para una categoria sin prioridades definidas,
+    en un circuito de 100 mesas.
+    La maxima prioridad para la seccion rige hasta el 2% con un minimo de 7 mesas.
+    """
+
+    definir_prioridades_seccion_categoria(settings)
+    
+    mesa_categoria = MesaCategoriaFactory(
+        categoria=categoria_standard(), mesa=mesa_en_seccion(seccion_prioritaria()))
     prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
 
     assert(prioridades.valor_para(proporcion,orden_de_llegada)) == prioridad
