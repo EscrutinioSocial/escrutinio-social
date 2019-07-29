017cf97ec827ba922f29025ea2404da4ad3e6940
Carlos Lombardi <carlombardi@gmail.com>
2019-07-28 21:10:33 -0300

    scheduling revisited - tests prioridades para MesaCategoria completos

--- scheduling/tests/test_models.py
+++ scheduling/tests/test_models.py
@@ -151,7 +151,7 @@ def test_prioridades_categoria(db, settings):
     [10, 11, 10000],
     [80, 81, 10000],
 ])
-def test_prioridades_mesa_categoria_standard(db, settings, proporcion, orden_de_llegada, prioridad):
+def test_prioridades_mesacat_standard(db, settings, proporcion, orden_de_llegada, prioridad):
     """
     Prioridades para una MesaCategoria para la cual ni la sección ni la categoría tienen 
     asignadas prioridades distintas a las standard
@@ -166,10 +166,6 @@ def test_prioridades_mesa_categoria_standard(db, settings, proporcion, orden_de_
     assert(prioridades.valor_para(proporcion,orden_de_llegada)) == prioridad
 
 
-    # PrioridadSchedulingFactory(seccion=seccion_prioritaria, desde_proporcion=0,
-    #                            hasta_proporcion=2, hasta_cantidad=7, prioridad=10)
-    # PrioridadSchedulingFactory(seccion=seccion_prioritaria, desde_proporcion=2, hasta_proporcion=10, prioridad=50)
-    # PrioridadSchedulingFactory(seccion=seccion_prioritaria, desde_proporcion=10, hasta_proporcion=100, prioridad=80)
 @pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
     [0, 1, 1000],
     [1, 2, 1000],
@@ -180,13 +176,12 @@ def test_prioridades_mesa_categoria_standard(db, settings, proporcion, orden_de_
     [10, 11, 8000],
     [80, 81, 8000],
 ])
-def test_prioridades_mesa_categoria_seccion_prioritaria_100(db, settings, proporcion, orden_de_llegada, prioridad):
+def test_prioridades_mesacat_seccion_prioritaria_100(db, settings, proporcion, orden_de_llegada, prioridad):
     """
     Prioridades para una MesaCategoria de una seccion prioritaria, para una categoria sin prioridades definidas,
     en un circuito de 100 mesas.
     La maxima prioridad para la seccion rige hasta el 2% con un minimo de 7 mesas.
     """
-
     definir_prioridades_seccion_categoria(settings)
     
     mesa_categoria = MesaCategoriaFactory(
@@ -194,3 +189,225 @@ def test_prioridades_mesa_categoria_seccion_prioritaria_100(db, settings, propor
     prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
 
     assert(prioridades.valor_para(proporcion,orden_de_llegada)) == prioridad
+
+
+@pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
+    [0, 1, 1000],
+    [0, 2, 1000],
+    [1, 15, 1000],
+    [1, 20, 1000],
+    [2, 21, 5000],
+    [7, 80, 5000],
+    [9, 100, 5000],
+    [10, 101, 8000],
+    [80, 801, 8000],
+])
+def test_prioridades_mesacat_seccion_prioritaria_1000(db, settings, proporcion, orden_de_llegada, prioridad):
+    """
+    Prioridades para una MesaCategoria de una seccion prioritaria, para una categoria sin prioridades definidas,
+    en un circuito de 1000 mesas.
+    La maxima prioridad para la seccion rige hasta el 2% con un minimo de 7 mesas.
+    """
+    definir_prioridades_seccion_categoria(settings)
+
+    mesa_categoria = MesaCategoriaFactory(
+        categoria=categoria_standard(), mesa=mesa_en_seccion(seccion_prioritaria()))
+    prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
+
+    assert(prioridades.valor_para(proporcion, orden_de_llegada)) == prioridad
+
+
+@pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
+    [0, 1, 10],
+    [1, 2, 10],
+    [2, 3, 100],
+    [4, 5, 100],
+    [9, 10, 100],
+    [10, 11, 500],
+    [80, 81, 500],
+])
+def test_prioridades_mesacat_categoria_prioritaria(db, settings, proporcion, orden_de_llegada, prioridad):
+    """
+    Prioridades para una MesaCategoria de una seccion standard, para una categoria prioritaria,
+    en un circuito de 100 mesas.
+    """
+    definir_prioridades_seccion_categoria(settings)
+
+    mesa_categoria = MesaCategoriaFactory(
+        categoria=categoria_pv(), mesa=mesa_en_seccion(seccion_standard()))
+    prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
+
+    assert(prioridades.valor_para(proporcion, orden_de_llegada)) == prioridad
+
+
+@pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
+    [0, 1, 50],
+    [1, 2, 50],
+    [2, 3, 50],
+    [6, 7, 50],
+    [7, 8, 250],
+    [9, 10, 250],
+    [10, 11, 400],
+    [80, 81, 400],
+])
+def test_prioridades_mesacat_categoria_y_seccion_prioritarias_100(db, settings, proporcion, orden_de_llegada, prioridad):
+    """
+    Prioridades para una MesaCategoria de una seccion prioritaria, para una categoria prioritaria,
+    en un circuito de 100 mesas.
+    La maxima prioridad para la seccion rige hasta el 2% con un minimo de 7 mesas.
+    """
+    definir_prioridades_seccion_categoria(settings)
+
+    mesa_categoria = MesaCategoriaFactory(
+        categoria=categoria_pv(), mesa=mesa_en_seccion(seccion_prioritaria()))
+    prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
+
+    assert(prioridades.valor_para(proporcion, orden_de_llegada)) == prioridad
+
+
+@pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
+    [0, 1, 50],
+    [0, 2, 50],
+    [1, 15, 50],
+    [2, 25, 250],
+    [7, 85, 250],
+    [9, 100, 250],
+    [10, 101, 400],
+    [85, 855, 400],
+])
+def test_prioridades_mesacat_categoria_y_seccion_prioritarias_1000(db, settings, proporcion, orden_de_llegada, prioridad):
+    """
+    Prioridades para una MesaCategoria de una seccion prioritaria, para una categoria prioritaria,
+    en un circuito de 100 mesas.
+    La maxima prioridad para la seccion rige hasta el 2% con un minimo de 7 mesas.
+    """
+    definir_prioridades_seccion_categoria(settings)
+
+    mesa_categoria = MesaCategoriaFactory(
+        categoria=categoria_pv(), mesa=mesa_en_seccion(seccion_prioritaria()))
+    prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
+
+    assert(prioridades.valor_para(proporcion, orden_de_llegada)) == prioridad
+
+
+@pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
+    [0, 1, 600],
+    [1, 2, 600],
+    [2, 3, 600],
+    [7, 8, 600],
+    [9, 10, 600],
+    [10, 11, 600],
+    [11, 12, 600],
+    [12, 13, 2400],
+    [29, 30, 2400],
+    [30, 31, 3600],
+    [40, 41, 3600],
+    [60, 61, 7500],
+    [80, 81, 7500],
+])
+def test_prioridades_mesacat_seccion_cuatro_prioridades_100(db, settings, proporcion, orden_de_llegada, prioridad):
+    """
+    Prioridades para una MesaCategoria de una seccion que tiene definidas cuatro niveles de prioridad en lugar de tres, para una categoria intermedia,
+    en un circuito de 100 mesas.
+    La maxima prioridad para la seccion rige hasta el 2% con un minimo de 12 mesas.
+    """
+    definir_prioridades_seccion_categoria(settings)
+
+    mesa_categoria = MesaCategoriaFactory(
+        categoria=categoria_gv(), mesa=mesa_en_seccion(seccion_cuatro_prioridades()))
+    prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
+
+    assert(prioridades.valor_para(proporcion, orden_de_llegada)) == prioridad
+
+
+@pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
+    [0, 1, 600],
+    [0, 7, 600],
+    [1, 12, 600],
+    [1, 18, 600],
+    [2, 23, 600],
+    [7, 84, 600],
+    [9, 100, 600],
+    [10, 101, 2400],
+    [11, 112, 2400],
+    [12, 123, 2400],
+    [29, 300, 2400],
+    [30, 301, 3600],
+    [40, 401, 3600],
+    [60, 601, 7500],
+    [80, 801, 7500],
+])
+def test_prioridades_mesa_seccion_cuatro_prioridades_1000(db, settings, proporcion, orden_de_llegada, prioridad):
+    """
+    Prioridades para una MesaCategoria de una seccion que tiene definidas cuatro niveles de prioridad en lugar de tres, para una categoria intermedia,
+    en un circuito de 1000 mesas.
+    La maxima prioridad para la seccion rige hasta el 2% con un minimo de 12 mesas.
+    """
+    definir_prioridades_seccion_categoria(settings)
+
+    mesa_categoria = MesaCategoriaFactory(
+        categoria=categoria_gv(), mesa=mesa_en_seccion(seccion_cuatro_prioridades()))
+    prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
+
+    assert(prioridades.valor_para(proporcion, orden_de_llegada)) == prioridad
+
+
+@pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
+    [0, 1, 200],
+    [1, 2, 200],
+    [2, 3, 400],
+    [3, 4, 400],
+    [4, 5, 400],
+    [5, 6, 2000],
+    [9, 10, 2000],
+    [10, 11, 10000],
+    [11, 12, 10000],
+    [19, 20, 10000],
+    [20, 21, 10000],
+    [40, 41, 10000],
+    [60, 61, 10000],
+])
+def test_prioridades_mesacat_seccion_con_definicion_parcial_1(db, settings, proporcion, orden_de_llegada, prioridad):
+    """
+    Prioridades para una MesaCategoria de una seccion que define prioridades distintas de las standard sólo para un rango de proporciones.
+    """
+    definir_prioridades_seccion_categoria(settings)
+    seccion_parcial = SeccionFactory(nombre="Definición parcial de prioridades no standard")
+    PrioridadSchedulingFactory(seccion=seccion_parcial, desde_proporcion=2, hasta_proporcion=5, prioridad=4)
+
+    mesa_categoria = MesaCategoriaFactory(
+        categoria=categoria_standard(), mesa=crear_mesa(seccion_parcial))
+    prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
+
+    assert(prioridades.valor_para(proporcion, orden_de_llegada)) == prioridad
+
+
+@pytest.mark.parametrize('proporcion, orden_de_llegada, prioridad', [
+    [0, 1, 200],
+    [1, 2, 200],
+    [2, 3, 2000],
+    [3, 4, 2000],
+    [4, 5, 2000],
+    [5, 6, 4000],
+    [9, 10, 4000],
+    [10, 11, 4000],
+    [11, 12, 4000],
+    [19, 20, 4000],
+    [20, 21, 10000],
+    [40, 41, 10000],
+    [60, 61, 10000],
+])
+def test_prioridades_mesacat_seccion_con_definicion_parcial_2(db, settings, proporcion, orden_de_llegada, prioridad):
+    """
+    Prioridades para una MesaCategoria de una seccion que define prioridades distintas de las standard sólo para un rango de proporciones.
+    Variante de test_prioridades_mesacat_seccion_con_definicion_parcial_1
+    """
+    definir_prioridades_seccion_categoria(settings)
+    seccion_parcial = SeccionFactory(nombre="Definición parcial de prioridades no standard")
+    PrioridadSchedulingFactory(seccion=seccion_parcial, desde_proporcion=5, hasta_proporcion=20, prioridad=40)
+
+    mesa_categoria = MesaCategoriaFactory(
+        categoria=categoria_standard(), mesa=crear_mesa(seccion_parcial))
+    prioridades = mapa_prioridades_para_mesa_categoria(mesa_categoria)
+
+    assert(prioridades.valor_para(proporcion, orden_de_llegada)) == prioridad
