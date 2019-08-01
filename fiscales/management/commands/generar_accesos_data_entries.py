11a861ae1bccab0371a0a0aa9df8b5fe185ba931
Fernando Schapachnik <fschapachnik@fundacionsadosky.org.ar>
2019-08-01 13:59:47 -0300

    Dado que muchos fiscales provinciales desde UB no van a cargar de una sección específica pero sí desde una provincia específica se agrega esa información opcional al fiscal para que se use para precompletar.

--- fiscales/management/commands/generar_accesos_data_entries.py
+++ fiscales/management/commands/generar_accesos_data_entries.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from fiscales.models import Fiscal
-from elecciones.models import Seccion
+from elecciones.models import Seccion, Distrito
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.contrib.auth.models import Group
@@ -13,10 +13,11 @@ class Command(BaseCommand):
         parser.add_argument('--prefijo', default='usr', type=str, help='Prefijo de los usuarios al que se agrega _ y el nro de serie.')
         parser.add_argument('--cantidad', type=int, help='Cantidad de usuarios a crear.')
         parser.add_argument('--grupo', default='fiscales con acceso al bot', type=str, help='En qué grupo se agregan.')
-        parser.add_argument('--id_seccion', type=int, help='id de la sección a la que corresponde el usuario.')
+        parser.add_argument('--id_seccion', type=int, help='Id de la sección a la que corresponde el usuario.')
+        parser.add_argument('--id_distrito', type=int, help='Id del distrito a la que corresponde el usuario.')
 
 
-    def crear_acceso(self, prefijo, indice, grupo, seccion, sobre_escribir=True):
+    def crear_acceso(self, prefijo, indice, grupo, distrito, seccion, sobre_escribir=True):
         username = slugify(f'{prefijo}_{indice:02d}')
 
         user, created = User.objects.get_or_create(username=username)
@@ -35,6 +36,7 @@ class Command(BaseCommand):
         fiscal.email_confirmado = True
         fiscal.apellido = username
         fiscal.nombres = username
+        fiscal.distrito = distrito
         fiscal.seccion = seccion
         fiscal.save()
 
@@ -51,10 +53,15 @@ class Command(BaseCommand):
         nombre_grupo = options['grupo']
         grupo = Group.objects.get(name=nombre_grupo)
         id_seccion = options['id_seccion']
-        seccion = Seccion.objects.get(id=id_seccion)
+        seccion = Seccion.objects.get(id=id_seccion) if id_seccion else None
+        id_distrito = options['id_distrito']
+        distrito = Distrito.objects.get(id=id_distrito) if id_distrito else None
 
         for i in range(cantidad):
-            user, clave = self.crear_acceso(prefijo=prefijo, indice=i, grupo=grupo, seccion=seccion, sobre_escribir=True)
+            user, clave = self.crear_acceso(
+                prefijo=prefijo, indice=i, grupo=grupo,
+                distrito=distrito, seccion=seccion, sobre_escribir=True
+            )
             self.stdout.write(self.style.SUCCESS(f'Usuario {user}, clave "{clave}" (sin las comillas).'))
 
 
