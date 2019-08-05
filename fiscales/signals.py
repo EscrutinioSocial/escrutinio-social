66437a9142e0cdcea6024b7ec611745638c8dd80
Martin Gaitan <mgaitan@onapsis.com>
2019-08-05 16:49:31 -0300

    fix user.fiscal solo si existe user

--- fiscales/signals.py
+++ fiscales/signals.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import user_logged_in, user_logged_out
 from django.dispatch import receiver
-from fiscales.models import Fiscal
+
 
 @receiver(user_logged_in)
 def on_user_logged_in(sender, request, **kwargs):
@@ -9,10 +9,11 @@ def on_user_logged_in(sender, request, **kwargs):
     # Me guardo la session key.
     fiscal.update_session_key(request.session.session_key)
 
+
 @receiver(user_logged_out)
 def on_user_logged_out(sender, **kwargs):
     user = kwargs.get('user')
-    fiscal = user.fiscal
-    # Deslogueado.
-    fiscal.update_session_key(None)
-
+    if user:
+        fiscal = user.fiscal
+        # Deslogueado.
+        fiscal.update_session_key(None)
