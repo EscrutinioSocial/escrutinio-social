ae83e02ba2b14ff62a7b00f5ca6dfa0eeb9f2068
Leo Orellana <leo.orellana5@gmail.com>
2019-08-07 11:55:18 -0300

    fix test carga de datos desde ub

--- fiscales/tests/test_carga_datos.py
+++ fiscales/tests/test_carga_datos.py
@@ -355,11 +355,21 @@ def test_cargar_resultados_mesa_desde_ub_con_id_de_mesa(
     """
     categoria_1 = CategoriaFactory()
     categoria_2 = CategoriaFactory()
-    mesa = MesaFactory(categorias=[categoria_1, categoria_2])
-    AttachmentFactory(mesa=mesa)
 
-    MesaCategoriaFactory(categoria=categoria_1, mesa=mesa, orden_de_carga=1)
-    MesaCategoriaFactory(categoria=categoria_2, mesa=mesa, orden_de_carga=2)
+    mesa = MesaFactory(categorias=[categoria_1, categoria_2])
+
+    mesa_categoria_1 = MesaCategoriaFactory(mesa=mesa, categoria=categoria_1, orden_de_carga=1)
+    mesa_categoria_2 = MesaCategoriaFactory(mesa=mesa, categoria=categoria_2, orden_de_carga=2)
+
+    opcion_1 = OpcionFactory(orden=1)
+    opcion_2 = OpcionFactory(orden=2)
+
+    CategoriaOpcionFactory(categoria=categoria_1, opcion=opcion_1, prioritaria=True)
+    CategoriaOpcionFactory(categoria=categoria_1, opcion=opcion_2, prioritaria=True)
+    CategoriaOpcionFactory(categoria=categoria_2, opcion=opcion_1, prioritaria=True)
+    CategoriaOpcionFactory(categoria=categoria_2, opcion=opcion_2, prioritaria=True)
+
+    AttachmentFactory(mesa=mesa)
 
     IdentificacionFactory(
         mesa=mesa,
@@ -375,11 +385,6 @@ def test_cargar_resultados_mesa_desde_ub_con_id_de_mesa(
     categoria_1.nombre = nombre_categoria
     categoria_1.save(update_fields=['nombre'])
 
-    opcion1 = categoria_1.opciones_actuales().get(nombre="opc1")
-    opcion2 = categoria_1.opciones_actuales().get(nombre="opc2")
-    opcion3 = categoria_2.opciones_actuales().get(nombre="opc1")
-    opcion4 = categoria_2.opciones_actuales().get(nombre="opc2")
-
     url_carga = reverse('procesar-acta-mesa', kwargs={'mesa_id': mesa.id})
     response = fiscal_client.get(url_carga)
 
@@ -389,36 +394,32 @@ def test_cargar_resultados_mesa_desde_ub_con_id_de_mesa(
     # categoria1 debería aparecer primero porque su mesa categoria tiene un orden_de_carga más grande
     assert nombre_categoria in str(response.content)
 
-    tupla_opciones_electores = [(opcion1.id, mesa.electores // 2, mesa.electores // 2), (opcion2.id, mesa.electores // 2, mesa.electores // 2)]
+    tupla_opciones_electores = [(opcion_1.id, mesa.electores // 2, mesa.electores // 2), (opcion_2.id, mesa.electores // 2, mesa.electores // 2)]
     request_data = _construir_request_data_para_carga_de_resultados(tupla_opciones_electores)
-
-    with django_assert_num_queries(38):
+    with django_assert_num_queries(58):
         response = fiscal_client.post(url_carga, request_data)
 
     # tiene otra categoría, por lo que debería cargar y redirigirnos nuevamente a procesar-acta-mesa
     carga = Carga.objects.get()
-
     assert carga.tipo == Carga.TIPOS.total
     assert response.status_code == HTTPStatus.FOUND
     assert response.url == reverse('procesar-acta-mesa', kwargs={'mesa_id': mesa.id})
-
-    tupla_opciones_electores = [(opcion3.id, mesa.electores // 2), (opcion4.id, mesa.electores // 2)]
 
     response = fiscal_client.post(url_carga, request_data)
 
     consumir_novedades_y_actualizar_objetos([carga.mesa_categoria])
 
-    # ya no tiene más categorías, debe dirigirnos a subir-adjunto
     cargas = Carga.objects.all()
     assert len(cargas) == 2
     assert carga.tipo == Carga.TIPOS.total
     assert response.status_code == HTTPStatus.FOUND
     assert response.url == reverse('procesar-acta-mesa', kwargs={'mesa_id': mesa.id})
 
-    response = fiscal_client.post(url_carga)
+    # ya no tiene más categorías, debe dirigirnos a subir-adjunto
+    response = fiscal_client.get(url_carga)
 
-    # la mesa no tiene más categorías, nos devuelve a la pantalla de carga de adjuntos
-    assert response.url == reverse('agregar-adjuntos-ub')
+    # # la mesa no tiene más categorías, nos devuelve a la pantalla de carga de adjuntos
+    assert reverse('agregar-adjuntos-ub') in str(response.content)
 
 
 def test_elegir_acta_mesas_con_id_inexistente_de_mesa_desde_ub(fiscal_client):
