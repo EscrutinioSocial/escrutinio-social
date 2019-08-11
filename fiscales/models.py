65be13825aaeaba1b945c9cd827bdf46a9c73b82
Fernando Schapachnik <fschapachnik@fundacionsadosky.org.ar>
2019-08-11 19:35:44 -0300

    fix

--- fiscales/models.py
+++ fiscales/models.py
@@ -253,8 +253,8 @@ class Fiscal(models.Model):
 
     def natural_key(self):
         return (self.tipo_dni, self.dni)
-    natural_key.dependencies = ['elecciones.distrito', 'elecciones.seccion']
-    
+    natural_key.dependencies = ['elecciones.distrito', 'elecciones.seccion', 'auth.user']    
+
 @receiver(post_save, sender=Fiscal)
 def crear_user_y_codigo_para_fiscal(sender, instance=None, created=False, **kwargs):
     """
