207d96886e0dd9bd2f576959b94667161087ea3c
Fernando Schapachnik <fschapachnik@fundacionsadosky.org.ar>
2019-09-01 20:35:23 -0300

    Se comienza a desacoplar la importación de las opciones, categorías, y categorías generales.

--- elecciones/management/commands/importar_categorias.py
+++ elecciones/management/commands/importar_categorias.py
@@ -4,15 +4,11 @@ from django.db.models import Q
 from django.db.utils import IntegrityError
 from pathlib import Path
 from csv import DictReader
-from elecciones.models import Partido, Opcion, Categoria, CategoriaOpcion, Mesa, MesaCategoria
+from elecciones.models import CategoriaGeneral, Categoria, CategoriaOpcion, Mesa, MesaCategoria
 import datetime
 from django.db import transaction
 
 from .basic_command import BaseCommand
-
-CSV_NACIONAL = 'categorias_nacionales.csv'
-CSV_PROVINCIAL = 'categorias_provinciales.csv'
-CSV_DISTRITAL = 'categorias_distrito_02.csv'
 
 PRIORIDAD_DEFAULT = 20000
 
@@ -20,40 +16,22 @@ PRIORIDAD_DEFAULT = 20000
 class Command(BaseCommand):
     """
     Formato:
-        partido_nombre,partido_nombre_corto,partido_codigo,partido_color,opcion_nombre,opcion_nombre_corto,partido_orden,opcion_orden,categoria_nombre
-        FRENTE DE TODOS,FRENTE DE TODOS,136,,CELESTE Y BLANCA A,CELESTE Y BLANCA A,1,1,Presidente y Vicepresidente
+        XXX Arreglar.
     """
-    help = "Importar categorías nacionales, creando partidos, opciones y asociando mesas"
+    help = "Importar categorías asociando mesas."
 
-    def add_arguments(self, parser):
-        super().add_argument(parser)
-        parser.add_argument('--provinciales',
-                            action="store_true",
-                            default=False,
-                            help='Indica si importa nacionales o distritales.'
-        )
-        parser.add_argument('--distritales',
-                            action="store_true",
-                            default=False,
-                            help='Indica si importa distritales.')
-
-
-    def crear_o_actualizar_categoria(self, categoria_slug, linea, row):
+    def crear_o_actualizar_categoria(self, linea, row):
         """
         Devuelve la categoría creada o encontrada.
         """
 
-        seccion_de_la_cat = None
+        distrito_nro = self.to_nat(row, 'distrito_nro', linea)
+        seccion_nro = self.to_nat(row, 'seccion_nro', linea)
+        secciones_list = row['secciones_list']
 
-        if self.provinciales or self.distritales:
-            distrito_nro = self.to_nat(row, 'distrito_nro', linea)
-            if distrito_nro is None:
-                return None
-
-        if self.distritales:
-            seccion_de_la_cat = row['municipio/sección']
-            secciones = row['secciones_list'].split(',')
-            ## para evitar iterar dos veces por secciones. Cómo te extraño sequence!
+        if secciones_list:
+            secciones = secciones_list.split(',')
+            # Para evitar iterar dos veces por secciones. Cómo te extraño sequence!
             secciones_nat = []
             for s in secciones:
                 s = self.to_nat_value(s, 'secciones_list', linea)
@@ -61,17 +39,16 @@ class Command(BaseCommand):
                     secciones_nat = []
                     break
                 secciones_nat.append(s)
-            if secciones_nat == []:
-                return None
 
-        categoria_nombre = row['categoria_nombre']
+        categoria_slug = row['slug']
+        categoria_nombre = row['nombre']
         categoria_general = CategoriaGeneral.objects.get(slug=row['categoriageneral_slug'])
         defaults = {
             'prioridad': prioridad_categoria,
             'nombre': categoria_nombre,
             'categoria_general': categoria_general,
             'distrito': Distrito.objects.get(numero=distrito_nro) if distrito_nro else None
-            'seccion': Seccion.objects.get(numero=seccion_de_la_cat, distrito__numero=distrito_nro) if seccion_de_la_cat else None
+            'seccion': Seccion.objects.get(numero=seccion_nro, distrito__numero=distrito_nro) if seccion_nro else None
         }
         try:
             categoria, created = Categoria.objects.update_or_create(
@@ -102,21 +79,21 @@ class Command(BaseCommand):
 
         prioridad_categoria = self.to_nat(row, 'categoria_prioridad', linea)
         if prioridad_categoria is None:
-            self.warning(f'La opción {codigo} no define la prioridad de la categoría o no es un natural.'
+            self.warning(f'La categoría {categoria_slug} no define la prioridad de la categoría o no es un natural.'
                          f' Se setea en el valor por defecto {PRIORIDAD_DEFAULT}. Línea {linea}.'
             )
             prioridad_categoria = PRIORIDAD_DEFAULT
 
         lookup = Q()
         # Se asocian las mesas a la categoría sólo cuando se crea la categoría.
-        if self.provinciales:
+        if distrito_nro and not secciones_nat:
             lookup &= Q(circuito__seccion__distrito__numero=distrito_nro)
-        if self.distritales:
+        elif distrito_nro and secciones_nat:
             lookup &= Q(circuito__seccion__distrito__numero=distrito_nro,
                         circuito__seccion__numero__in=secciones_nat
             )
 
-        # sólo excluimos si la categoría es xenófoba...
+        # Sólo excluimos si la categoría es xenófoba...
         if not extranjeros:
             lookup &= Q(extranjeros=extranjeros)
         mesas = Mesa.objects.filter(lookup)
@@ -133,90 +110,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         super().handle(*args, **options)
 
-        self.provinciales = options['provinciales']
-        self.distritales = options['distritales']
-        self.nacionales = not provinciales and not distritales
+        reader = DictReader(self.CSV.open())
 
-        archivo = self.CSV / CSV_PROVINCIAL if self.provinciales else CSV_DISTRITAL if self.distritales else CSV_NACIONAL
+        for linea, row in enumerate(reader, 1):
 
-        reader = DictReader(archivo.open())
-        errores = []
+            self.crear_o_actualizar_categoria(linea, row)
 
-        categoria_slug_anterior = None
-        for linea row in enumerate(reader, 1):
-
-            # Categoría
-            categoria_slug = row['categoria_slug']
-            if categoria_slug != categoria_slug_anterior:
-                categoria_generada = self.crear_o_actualizar_categoria(categoria_slug, linea, row)
-                categoria_slug_anterior = categoria_slug
-                if not categoria_generada:
-                    continue
-                categoria = categoria_generada
-
-            # Partido.
-            codigo = row['partido_codigo']
-            nombre = row['partido_nombre']
-            nombre_corto = row['partido_nombre_corto'][:30]
-            color = row['partido_color']
-
-            partido_orden = self.to_nat(row, 'partido_orden', linea)
-            if partido_orden is None:
-                continue
-
-            defaults = {
-                'nombre': nombre,
-                'nombre_corto': nombre_corto,
-                'color': color,
-                'orden': partido_orden,
-            }
-            partido, created = Partido.objects.update_or_create(codigo=codigo, defaults=defaults)
-
-            self.log_creacion(partido, created)
-
-            # Opción.
-            nombre = row['opcion_nombre']
-
-            orden = self.to_nat(row, 'opcion_orden', linea)
-            if orden is None:
-                orden = partido_orden
-                self.warning(f'Usando orden del partido para {nombre}. Línea {linea}.')
-
-            ## Realmente queremos cortar así?
-            nombre_corto = row['opcion_nombre_corto'][:20]
-            opcion_codigo = row.get('opcion_codigo', None)
-            opcion_codigo = opcion_codigo if opcion_codigo else codigo  # Si no tiene uso el del partido.
-            defaults = {
-                'nombre': nombre,
-                'nombre_corto': nombre_corto,
-            }
-
-            try:
-                opcion, created = Opcion.objects.update_or_create(partido=partido,
-                                                                  codigo=opcion_codigo,
-                                                                  defaults=defaults
-                )
-                if opcion is None:
-                    self.error_log(f'No se pudo crear la opción {nombre}. Línea {linea}.')
-            except Opcion.MultipleObjectsReturned:
-                # Si hay varias las borro y creo una nueva.
-                Opcion.objects.filter(partido=partido, codigo=opcion_codigo).delete()
-                self.warning(f'La opción {nombre} estaba repetida. Borramos y volvemos a crear. Línea {linea}.')
-                opcion, created = Opcion.objects.update_or_create(partido=partido,
-                                                                  codigo=opcion_codigo,
-                                                                  defaults=defaults
-                )
-
-            self.log_creacion(opcion, created)
-
-
-            # Categoría-Opción.
-            defaults = {
-                'orden': orden,
-            }
-            categoriaopcion, created = CategoriaOpcion.objects.get_or_create(
-                categoria=categoria,
-                opcion=opcion,
-                defaults=defaults
-            )
-            self.log_creacion(categoriaopcion, created)
---
+++ elecciones/management/commands/importar_categorias_generales.py
@@ -0,0 +1,56 @@
+from decimal import Decimal
+from django.conf import settings
+from django.db.models import Q
+from django.db.utils import IntegrityError
+from pathlib import Path
+from csv import DictReader
+from elecciones.models import CategoriaGeneral
+import datetime
+from django.db import transaction
+
+from .basic_command import BaseCommand
+
+PRIORIDAD_DEFAULT = 20000
+
+
+class Command(BaseCommand):
+    """
+    Formato:
+        XXX Arreglar.
+    """
+    help = "Importar categorías generales."
+
+    def crear_o_actualizar_categoria_general(self, linea, row):
+        """
+        Devuelve la categoría creada o encontrada.
+        """
+
+        categoria_slug = row['slug']
+        categoria_nombre = row['nombre']
+        defaults = {
+            'nombre': categoria_nombre,
+        }
+        try:
+            categoria, created = CategoriaGeneral.objects.update_or_create(
+                slug=categoria_slug,
+                defaults=defaults
+            )
+        except IntegrityError:
+            self.error_log(f'El slug {categoria_slug} ya estaba en uso. No se crea la '
+                           f'categoría {categoria_nombre}. Línea {linea}.'
+            )
+            return None
+
+        self.log_creacion(categoria, created)
+
+        return categoria
+
+    def handle(self, *args, **options):
+        super().handle(*args, **options)
+
+        reader = DictReader(self.CSV.open())
+
+        for linea, row in enumerate(reader, 1):
+
+            self.crear_o_actualizar_categoria_general(linea, row)
+
