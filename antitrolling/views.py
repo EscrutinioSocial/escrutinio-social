9ba633e8ea057b38854855f6237da419a022b37c
Carlos Lombardi <carlombardi@gmail.com>
2019-09-15 12:11:05 -0300

    rangos de porcentaje con indicadores peligro - el modelo está organizado

--- antitrolling/views.py
+++ antitrolling/views.py
@@ -1,3 +1,5 @@
+import math
+
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
@@ -38,7 +40,79 @@ class MonitorAntitrolling(TemplateView):
     template_name = "antitrolling/monitoreo_antitrolling.html"
 
     def get_context_data(self, **kwargs):
+        ParametrosAntitrolling.reset()
         context = super().get_context_data(**kwargs)
-        context['fiscales'] = Fiscal.objects.count()
-        context['umbral_troll'] = config.SCORING_MINIMO_PARA_CONSIDERAR_QUE_FISCAL_ES_TROLL
+        context['umbral_troll'] = ParametrosAntitrolling.umbral_troll
+        context['fiscales'] = ParametrosAntitrolling.cantidad_fiscales
         return context
+
+
+class ParametrosAntitrolling():
+    umbral_troll = None
+    cantidad_fiscales = None
+
+    @classmethod
+    def reset(cls):
+        cls.umbral_troll = config.SCORING_MINIMO_PARA_CONSIDERAR_QUE_FISCAL_ES_TROLL
+        cls.cantidad_fiscales = Fiscal.objects.count()
+
+
+class FraccionFiscalesPorScoringTroll():
+    def setRangoPorcentajes(self, desde_porcentaje, hasta_porcentaje):
+        self.desde_scoring = None if desde_porcentaje == None else floor(
+            ((ParametrosAntitrolling.umbral_troll * desde_porcentaje) / 100) + 1)
+        self.desde_porcentaje = desde_porcentaje
+        self.hasta_scoring = None if hasta_porcentaje == None else floor(
+            (ParametrosAntitrolling.umbral_troll * hasta_porcentaje) / 100)
+        self.hasta_porcentaje = desde_porcentaje
+        self.cantidad = None
+        return self
+
+    def set_indicador_de_peligro(self, indicador):
+        self.indicador_de_peligro = indicador
+        return self
+
+    def cantidad_fiscales(self):
+        self.calcular()
+        return self.cantidad
+
+    def porcentaje_fiscales(self):
+        self.calcular()
+        return (self.cantidad * 100) / ParametrosAntitrolling.umbral_troll
+
+    def indicador_peligro(self):
+        return self.indicador_de_peligro.indicador_peligro(self.porcentaje_fiscales())
+        
+    def calcular(self):
+        if (self.cantidad == None):
+            query = Fiscales.objects.filter(troll=False)
+            if (self.desde_scoring != None):
+                query = query.filter(puntaje_scoring_troll >= self.desde_scoring)
+            if (self.hasta_scoring != None):
+                query = query.filter(puntaje_scoring_troll <= self.hasta_scoring)
+            self.cantidad = query.count()
+
+
+class IndicadorDePeligro():
+    indicador_rojo = "rojo"
+    indicador_naranja = "naranja"
+    indicador_amarillo = "amarillo"
+    indicador_verde = "verde"
+
+    def set_umbrales(self, umbral_rojo, umbral_naranja, umbral_amarillo):
+        self.umbral_rojo = umbral_rojo
+        self.umbral_naranja = umbral_naranja
+        self.umbral_amarillo = umbral_amarillo
+        return self
+
+    def indicador_peligro(self, valor):
+        if (valor >= self.umbral_rojo):
+            return self.indicador_rojo
+        elif (valor >= self.umbral_naranja):
+            return self.indicador_naranja
+        elif (valor >= self.umbral_amarillo):
+            return self.indicador_amarillo
+        else:
+            return self.indicador_verde
+
+
