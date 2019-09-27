333b20f07013dda6cc74f7dd2f12d9e46be6d512
Fernando Schapachnik <fschapachnik@fundacionsadosky.org.ar>
2019-09-27 14:11:21 -0300

    Se agrega la posibilidad de generar usuarios para todo un distrito.

--- fiscales/management/commands/generar_accesos_data_entries.py
+++ fiscales/management/commands/generar_accesos_data_entries.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.contrib.auth.models import Group
 
+
 class Command(BaseCommand):
     help = """ Generar accesos para los usuarios del sistema.
             """
@@ -16,13 +17,13 @@ class Command(BaseCommand):
         parser.add_argument('--grupo', default='fiscales con acceso al bot', type=str, help='En qué grupo se agregan.')
         parser.add_argument('--nro_seccion', type=str, help='Nro de la sección a la que corresponde el usuario.')
         parser.add_argument('--nro_distrito', type=str, help='Nro del distrito a la que corresponde el usuario.')
-
+        parser.add_argument('--por_seccion', dest='por_seccion', action='store_true', default=False, help='Genera usuarios por sección dentro del distrito indicado.')
 
     def crear_acceso(self, prefijo, indice, grupo, distrito, seccion, sobre_escribir=True):
         username = slugify(f'{prefijo}{indice:03d}')
 
         user, created = User.objects.get_or_create(username=username)
-        user.is_staff= True
+        user.is_staff = False
         if created or sobre_escribir:
             password = User.objects.make_random_password().lower()[0:8]
             user.set_password(password)
@@ -46,6 +47,23 @@ class Command(BaseCommand):
 
         return user, password
 
+    def alta_usuarios(self, prefijo, indice_inicial, cantidad, distrito, seccion, grupo):
+        for i in range(indice_inicial, indice_inicial + cantidad):
+            user, clave = self.crear_acceso(
+                prefijo=prefijo, indice=i, grupo=grupo,
+                distrito=distrito, seccion=seccion, sobre_escribir=True
+            )
+            self.stdout.write(self.style.SUCCESS(f'{user},{clave}'))
+
+    def alta_usuarios_por_seccion(self, prefijo, indice_inicial, cantidad, distrito, grupo):
+        """
+        Da de alta usuarios en cada sección del distrito.
+        """
+        secciones = distrito.secciones.all()
+        for seccion in secciones:
+            nombre = seccion.nombre.lower().replace(' ', '')
+            self.alta_usuarios(f'{nombre}{prefijo}', indice_inicial, cantidad, distrito, seccion, grupo)
+
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('--- Generando usuarios ---'))
 
@@ -56,17 +74,13 @@ class Command(BaseCommand):
         grupo = Group.objects.get(name=nombre_grupo)
         nro_seccion = options['nro_seccion']
         nro_distrito = options['nro_distrito']
-        seccion = Seccion.objects.get(numero=nro_seccion, distrito__numero=nro_distrito) if nro_seccion and nro_distrito else None
         distrito = Distrito.objects.get(numero=nro_distrito) if nro_distrito else None
+        seccion = Seccion.objects.get(numero=nro_seccion, distrito__numero=nro_distrito) if nro_seccion and nro_distrito else None
 
-        for i in range(indice_inicial, indice_inicial + cantidad):
-            user, clave = self.crear_acceso(
-                prefijo=prefijo, indice=i, grupo=grupo,
-                distrito=distrito, seccion=seccion, sobre_escribir=True
-            )
-            self.stdout.write(self.style.SUCCESS(f'{user},{clave}'))
-
+        if options['por_seccion']:
+            self.alta_usuarios_por_seccion(prefijo, indice_inicial, cantidad, distrito, grupo)
+        else:
+            self.alta_usuarios(prefijo, indice_inicial, cantidad, distrito, seccion, grupo)
 
         self.stdout.write(self.style.SUCCESS('--- Terminado. ---'))
-
 
