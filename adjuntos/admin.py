6dc9c0e9676c4f3fdf854624438b0812cfdb98c2
Fernando Schapachnik <fschapachnik@fundacionsadosky.org.ar>
2019-09-27 12:34:28 -0300

    Se agrega ExportableAdmin para el listado de attachments.

--- adjuntos/admin.py
+++ adjuntos/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.shortcuts import reverse
 from .models import Attachment, Identificacion
 from django_admin_row_actions import AdminRowActionsMixin
+from django_exportable_admin.admin import ExportableAdmin
 
 
 class IdentificacionInline(admin.StackedInline):
@@ -10,7 +11,7 @@ class IdentificacionInline(admin.StackedInline):
     raw_id_fields = ('fiscal', 'mesa')
 
 
-class AttachmentAdmin(AdminRowActionsMixin, admin.ModelAdmin):
+class AttachmentAdmin(AdminRowActionsMixin, ExportableAdmin):
     list_display = ('status', 'mesa', 'foto', 'foto_edited', 'cant_fiscales_asignados', 'subido_por', 'pre_identificacion')
     list_filter = ('status',)
     search_fields = ('mesa__numero', 'subido_por__user__username')
