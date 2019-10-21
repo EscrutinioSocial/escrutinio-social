c5b42f21ecf4b81a0073411fd0643ef6ba903897
Fernando Schapachnik <fernando@schapachnik.com.ar>
2019-10-20 23:12:05 -0300

    Agrego columnas y ordering.
    Former-commit-id: 54530f2866f118fee974b7a767cfe0060f019e90

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -3,9 +3,13 @@ from django_admin_row_actions import AdminRowActionsMixin
 
 from .models import ColaCargasPendientes
 
+
 class ColaCargasPendientesAdmin(AdminRowActionsMixin, admin.ModelAdmin):
     model = ColaCargasPendientes
     extra = 0
+    ordering = ['orden']
     raw_id_fields = ('mesa_categoria', 'attachment', 'distrito', 'seccion')
+    list_display = ('id', 'orden', 'mesa_categoria', 'attachment', 'distrito', 'seccion')
+
 
 admin.site.register(ColaCargasPendientes, ColaCargasPendientesAdmin)
