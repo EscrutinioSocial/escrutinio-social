89ae93032c5b1ee39463413a559282b19cc7187a
Miguel Pagano <miguel.pagano@gmail.com>
2019-10-20 23:04:18 -0300

    Admin de Cola de Tareas Pendientes usable.
    Former-commit-id: 5d1897a9fbf94883238ed165d99eda45db1f4ab6

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from django_admin_row_actions import AdminRowActionsMixin
+
 from .models import ColaCargasPendientes
 
-admin.site.register(ColaCargasPendientes)
+class ColaCargasPendientesAdmin(AdminRowActionsMixin, admin.ModelAdmin):
+    model = ColaCargasPendientes
+    extra = 0
+    raw_id_fields = ('mesa_categoria', 'attachment', 'distrito', 'seccion')
+
+admin.site.register(ColaCargasPendientes, ColaCargasPendientesAdmin)
