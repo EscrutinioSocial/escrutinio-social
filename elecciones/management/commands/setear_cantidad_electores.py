6727482b7a3931efa29181908c2646e37ccbf236
Fernando Schapachnik <fernando@schapachnik.com.ar>
2019-10-25 16:04:42 -0300

    Se copian las categorías y la escuela de otra mesa del circuito. Además, se setea la presencia de electores.
    Former-commit-id: 3709473eaa793c65856a5c4cc65b5ea3bc6a427a

--- elecciones/management/commands/setear_cantidad_electores.py
+++ elecciones/management/commands/setear_cantidad_electores.py
@@ -16,15 +16,30 @@ class Command(BaseCommand):
     help = "Define la cantidad de electores por mesa."
 
     formato = ['distrito', 'seccion', 'circuito', 'mesa', 'numero']
-    
+
+    def copiar_mesa(self, circuito, mesa_nueva):
+        otra_mesa = Mesa.objects.filter(circuito=circuito).exclude(id=mesa_nueva.id).first()
+        if not otra_mesa:
+            self.warning(f"No hay mesas en circuito {circuito}.")
+            return
+
+        # Le copiamos las categorías.
+        for categoria in otra_mesa.categorias.all():
+            mesa_nueva.categoria_add(categoria)
+
+        # Y el lugar de votación.
+        mesa_nueva.lugar_de_votacion = otra_mesa.lugar_de_votacion
+        mesa_nueva.save(update_fields=['lugar_de_votacion'])
+
     def handle(self, *args, **options):
         super().handle(*args, **options)
 
-        reader = DictReader(self.CSV.open(),self.formato)
+        reader = DictReader(self.CSV.open(), self.formato)
         fallos = []
         for c, row in enumerate(reader, 1):
-            self.log(f"{row['distrito']}, {row['seccion']}, {row['circuito']}, {row['mesa']}, {row['numero']}",
-                     level=3
+            self.log(
+                f"{row['distrito']}, {row['seccion']}, {row['circuito']}, {row['mesa']}, {row['numero']}",
+                level=3
             )
 
             nro_distrito = canonizar(row['distrito'])
@@ -34,7 +49,7 @@ class Command(BaseCommand):
             cant = row['numero']
 
             mensaje_fallo_mesa = f'No se pudo procesar la línea {c}. ({nro_distrito},{nro_seccion},{nro_circuito},{nro_mesa})'
-            
+
             try:
                 distrito = Distrito.objects.get(numero=nro_distrito)
                 seccion = Seccion.objects.get(numero=nro_seccion, distrito=distrito)
@@ -53,24 +68,22 @@ class Command(BaseCommand):
                 fallos.append(fallo)
                 self.warning(fallo)
             except Mesa.DoesNotExist:
-                fallo = f'No existe la mesa {nro_mesa} en el circuito {circuito}. La creamos.'
-                mesa = Mesa(numero = nro_mesa,
-                            circuito = circuito,
-                            electores = cant
+                fallo = f'No existe la mesa {nro_mesa} en el circuito {circuito}-{seccion}-{distrito}. La creamos.'
+                
+                mesa = Mesa.objects.create(
+                    numero=nro_mesa,
+                    circuito=circuito,
+                    electores=cant
                 )
-                mesa.save()
                 fallos.append(fallo)
                 self.warning(fallo)
+                self.copiar_mesa(circuito, mesa)
             else:
-
                 mesa.electores = cant
-                mesa.save()
+                mesa.save(update_fields=['electores'])
 
-                self.log(f'Se actualizó la cantidad de la mesa {mesa.id}', level=3)
+                self.log(f'Se actualizó la cantidad de electores de la mesa {mesa.id}', level=3)
 
-            if c > 2200:
-                break
-                
         self.log(f'Se procesaron {c} líneas.', level=1)
         for fallo in fallos:
             self.error_log(fallo)
